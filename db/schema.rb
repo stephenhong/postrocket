f5c8a767dee4034342fc6da092225437d2d0421b
hearthstone <stephenhong@wiredhere.com>
2016-10-12 20:53:06 +0000

    admin panel done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006190823) do
+ActiveRecord::Schema.define(version: 20161012163624) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20161006190823) do
   end
 
   create_table "calendars", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "chats", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
